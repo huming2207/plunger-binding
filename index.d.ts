82b24d3a417b6cacb3a39496c1589dbe3837c240
Jackson Ming Hu <huming2207@gmail.com>
2021-06-13 16:07:15 +1000

    Publish 1.0.0

--- index.d.ts
+++ index.d.ts
@@ -16,4 +16,12 @@ export interface Probes {
     probes: ProbeInfo[]
 }
 
+export enum FirmwareType {
+    BIN = 'bin',
+    HEX = 'hex',
+    ELF = 'elf'
+}
+
 export const listAllProbes: () => Probes;
+export const eraseStm32L0: (targetName: string, vid: number, pid: number, serialNum?: String) => Promise<void>;
+export const flashFirmwareFile: (path: string, targetName: string, type: FirmwareType, vid: number, pid: number, skip_erase?: boolean, speed_khz?: number, serialNum?: string) => Promise<void>;
