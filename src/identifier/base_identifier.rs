49d081deeccbdc26bea866da1bc8f5c4f7491923
Jackson Ming Hu <jackson@smartguide.com.au>
2021-06-16 11:47:55 +1000

    identifier: add target identity struct

--- src/identifier/base_identifier.rs
+++ src/identifier/base_identifier.rs
@@ -1,6 +1,19 @@
+use std::usize;
+
 use crate::common::plunger_error::PlungerError;
+use serde::{Serialize, Deserialize};
+use serde_with::{serde_as, hex::Hex};
 
 pub trait BaseIdentifier {
-    fn get_uid(self) -> Result<u128, PlungerError>;
-    fn get_flash_size(self) -> Result<usize, PlungerError>;
+    fn get_uid(&self) -> Result<Vec<u8>, PlungerError>;
+    fn get_flash_size(&self) -> Result<usize, PlungerError>;
+}
+
+#[serde_as]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TargetIdentity {
+    #[serde_as(as = "Option<Hex>")]
+    pub unique_id: Option<Vec<u8>>,
+    pub flash_size: Option<usize>,
 }
